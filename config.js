58fc8ddaadc69c5f728cd91842d5cbb14cab6cce
RisedSky <risedsky@gmail.com>
2018-09-15 23:15:24 +0200

    fix

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    BOT_TOKEN: "process.env.BOT_TOKEN",
+    BOT_TOKEN: process.env.BOT_TOKEN,
     prefix: ">",
     bot_version: "1.0.0",
     DefaultGuildID: "450568803890364437",
